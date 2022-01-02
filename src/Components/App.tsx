d1af6d4d2660e1b6e4cb1ed4758efde8d4a078b0
seongjun_kim <lewisel@naver.com>
2022-01-02 14:39:08 +0900

    feat: 입력 상자 및 버튼 적용

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,59 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
-import Styled, { keyframes } from 'styled-components';
-
-const spin = keyframes`
-  from {
-    transform: rotate(0deg);
-  }
-  to {
-    transform: rotate(360deg);
-  }
-`;
+import Styled from 'styled-components';
+import { Button, Input } from 'Components';
 
 const Container = Styled.div`
-  text-align: center;
-`;
-
-const Header = Styled.div`
-  background-color: #282c34 !important;
   min-height: 100vh;
-  display: flex;
-  flex-direction: column;
+  display: flexx;
   align-items: center;
   justify-content: center;
-  font-size: calc(10px + 2vmin);
-  color: white;
+  flex-direction: column;
 `;
 
-const Image = Styled.img`
-  height: 40vmin;
-  pointer-events: none;
-  @media (prefers-reduced-motion: no-preference) {
-    // styled-components에서는 해당 컴포넌트가 직접 애니메이션을 
-    // 수행하므로 클래스명을 추가로 명시하지 않아도 된다!
-    // .App-logo {
-      animation: ${spin} infinite 20s linear;
-    // }
-  }
+const Contents = Styled.div`
+  display:flex;
+  background-color: #fff;
+  flex-direction: column;
+  padding: 20px;
+  border-radius: 8px;
+  box-shadow: 5px 5px 10px rgba(0,0,0,0.2);
 `;
 
-const AppLink = Styled.a`
-  color: #61dafb
+const InputContainer = Styled.div`
+  display: flex;
+  align-self: center;
+  width: 105%;
+  justify-content: space-around;
 `;
 
 function App() {
   return (
     <Container>
-      <Header>
-        <Image src={logo} alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <AppLink href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </AppLink>
-      </Header>
+      <Contents>
+        <InputContainer>
+          <Input placeholder="Enter what to do..." onChange={(text) => console.log(text)} />
+          <Button
+            label="Add"
+            onClick={() => {
+              alert('Add!');
+            }}
+          />
+        </InputContainer>
+        {/* <Button label="Delete" backgroundColor="#ff1744" hoverColor="#f01440" /> */}
+      </Contents>
     </Container>
   );
 }
