c3b9b6155cd49fc8565544b64c8f909c77b040a8
seongjun_kim <lewisel@naver.com>
2022-01-03 19:41:54 +0900

    feat: 앱 데이터 저장 (localStorage)
     localStorage에 앱 데이터를 저장해두고, useEffect 훅을 활용해 초기 렌더 직후 데이터를 로드해오도록 수정

--- src/Contexts/ToDoList/index.tsx
+++ src/Contexts/ToDoList/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, createContext } from 'react';
+import React, { useEffect, useState, createContext } from 'react';
 
 interface Context {
   readonly toDoList: string[];
@@ -21,15 +21,25 @@ const ToDoListProvider = ({ children }: Props): JSX.Element => {
 
   const addToDo = (toDo: string): void => {
     if (toDo) {
-      setToDoList([...toDoList, toDo]);
+      const newList = [...toDoList, toDo];
+      localStorage.setItem('ToDoList', JSON.stringify(newList));
+      setToDoList(newList);
     }
   };
 
   const deleteToDo = (index: number): void => {
     let list = [...toDoList];
     list.splice(index, 1);
+    localStorage.setItem('ToDoList', JSON.stringify(list));
     setToDoList(list);
   };
+
+  useEffect(() => {
+    const list = localStorage.getItem('ToDoList');
+    if (list) {
+      setToDoList(JSON.parse(list));
+    }
+  }, []);
 
   return (
     <ToDoListContext.Provider
