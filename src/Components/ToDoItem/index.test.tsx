24679cbecf65e910c3a49eb8bb74aaa145219a31
seongjun_kim <lewisel@naver.com>
2022-01-05 12:45:17 +0900

    chore: ToDoItem 컴포넌트 테스트 수정
     - renderWithRouter로 router wrapping
     - 링크를 통한 이동 테스트 추가

--- src/Components/ToDoItem/index.test.tsx
+++ src/Components/ToDoItem/index.test.tsx
@@ -3,10 +3,14 @@ import { render, screen, fireEvent } from '@testing-library/react';
 import 'jest-styled-components';
 
 import { ToDoItem } from './index';
+import { useLocation } from 'react-router-dom';
+import { renderWithRouter } from 'Libs/utils';
 
 describe('<ToDoItem />', () => {
   it('renders component correctly', () => {
-    const { container } = render(<ToDoItem id={0} label="default value" />);
+    const { container } = renderWithRouter(<ToDoItem id={0} label="default value" />, {
+      route: '/',
+    });
     const toDoItem = screen.getByText('default value');
     expect(toDoItem).toBeInTheDocument();
 
@@ -17,10 +21,32 @@ describe('<ToDoItem />', () => {
 
   it('clicks the delete button', () => {
     const handleClick = jest.fn();
-    render(<ToDoItem id={0} label="default value" onDelete={handleClick} />);
+    renderWithRouter(<ToDoItem id={0} label="default value" onDelete={handleClick} />, {
+      route: '/',
+    });
     const deleteButton = screen.getByText('Delete');
     expect(handleClick).toBeCalledTimes(0);
     fireEvent.click(deleteButton);
     expect(handleClick).toBeCalledTimes(1);
+  });
+
+  it('clicks the link to detail page', () => {
+    const TestComponent = (): JSX.Element => {
+      const { pathname } = useLocation();
+      return (
+        <div>
+          <div>{pathname}</div>
+          <ToDoItem id={0} label="default value" />
+        </div>
+      );
+    };
+
+    renderWithRouter(<TestComponent />, { route: '/' });
+    const pathName = screen.getByText('/');
+    expect(pathName).toBeInTheDocument();
+
+    const toDoItem = screen.getByText('default value');
+    fireEvent.click(toDoItem);
+    expect(pathName.textContent).toBe('/detail/0');
   });
 });
