2bc2ff227f284b46e148836446c072250537ed8f
seongjun_kim <lewisel@naver.com>
2022-01-06 16:33:40 +0900

    chore: App 테스트 수정
     - 앱 주요기능인 라우팅 및 데이터 추가/삭제 기능 확인을 위해 renderWithMemoryRouter 적용
     - 테스트 아이디로 확인 가능한 리스트의 경우, 데이터 변경 이후 확인을 위해 재할당으로 렌더사항 갱신

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,76 +1,164 @@
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { screen, fireEvent } from '@testing-library/react';
 import App from './App';
 import 'jest-styled-components';
+import { renderWithMemoryRouter } from 'Libs/utils';
+import { useLocation } from 'react-router-dom';
 
 describe('<App />', () => {
   it('renders component correctly', () => {
-    const { container } = render(<App />);
+    const { container } = renderWithMemoryRouter();
+
+    const header = screen.getByText('Todo List');
+    expect(header).toBeInTheDocument();
 
     const toDoList = screen.getByTestId('toDoList');
     expect(toDoList).toBeInTheDocument();
     expect(toDoList.firstChild).toBeNull();
 
-    const input = screen.getByPlaceholderText('Enter what to do...');
-    expect(input).toBeInTheDocument();
-    const label = screen.getByText('Add');
-    expect(label).toBeInTheDocument();
-
     expect(container).toMatchSnapshot();
   });
 
-  it('adds and deletes ToDo item', () => {
-    render(<App />);
+  it('goes to Add page and comes back to List page', () => {
+    const TestComponent = () => {
+      const { pathname } = useLocation();
+      return <div>{pathname}</div>;
+    };
+    const { container } = renderWithMemoryRouter({}, <TestComponent />);
 
-    // Add first ToDo item
+    const header = screen.getByText('Todo List');
+    const url = screen.getByText('/');
+
+    const plusButton = screen.getByText('+');
+    expect(plusButton).toBeInTheDocument();
+
+    fireEvent.click(plusButton);
+
+    expect(plusButton).not.toBeInTheDocument();
+    expect(header.textContent).toBe('Add Todo');
+    expect(url.textContent).toBe('/add');
+    const goBackButton = screen.getByText('Go Back...');
+    expect(goBackButton).toBeInTheDocument();
+    const addButton = screen.getByText('Add');
+    expect(addButton).toBeInTheDocument();
+    const input = screen.getByPlaceholderText('Enter what to do...');
+    expect(input).toBeInTheDocument();
+
+    expect(container).toMatchSnapshot();
+
+    fireEvent.click(goBackButton);
+    expect(header.textContent).toBe('Todo List');
+    const toDoList = screen.getByTestId('toDoList');
+    expect(toDoList).toBeInTheDocument();
+  });
+
+  it('adds a new todo in add page and comes back to List page', () => {
+    renderWithMemoryRouter();
+
+    let toDoList = screen.getByTestId('toDoList');
+    expect(toDoList.childNodes.length).toBe(0);
+
+    const plusButton = screen.getByText('+');
+    fireEvent.click(plusButton);
+
     const input = screen.getByPlaceholderText('Enter what to do...');
     const addButton = screen.getByText('Add');
-    fireEvent.change(input, { target: { value: '1st TODO ITEM' } });
+    fireEvent.change(input, { target: { value: 'New Todo' } });
     fireEvent.click(addButton);
-    const firstToDoItem = screen.getByText('1st TODO ITEM');
-    expect(firstToDoItem).toBeInTheDocument();
-    const toDoList = screen.getByTestId('toDoList');
-    expect(toDoList.childElementCount).toBe(1);
 
-    // Check delete button added in the first ToDO item
-    const deleteButton = screen.getByText('Delete');
-    expect(deleteButton).toBeInTheDocument();
-
-    // Add second ToDo item
-    fireEvent.change(input, { target: { value: '2nd TODO ITEM' } });
-    fireEvent.click(addButton);
-    const secondToDoItem = screen.getByText('2nd TODO ITEM');
-    expect(secondToDoItem).toBeInTheDocument();
-    expect(toDoList.childElementCount).toBe(2);
-
-    // Delete the first ToDo item
-    const deleteButtons = screen.getAllByText('Delete');
-    fireEvent.click(deleteButtons[0]);
-    expect(firstToDoItem).not.toBeInTheDocument();
-    expect(toDoList.childElementCount).toBe(1);
+    const header = screen.getByText('Todo List');
+    expect(header).toBeInTheDocument();
+    const newToDo = screen.getByText('New Todo');
+    expect(newToDo).toBeInTheDocument();
+    toDoList = screen.getByTestId('toDoList'); // testId로 조회한 ToDoList 컴포넌트의 렌더링 갱신
+    expect(toDoList.childNodes.length).toBe(1);
+    expect(localStorage.getItem('ToDoList')).toBe('["New Todo"]');
   });
 
-  it('does not add empty ToDo', () => {
-    render(<App />);
-
-    const toDoList = screen.getByTestId('toDoList');
-    const length = toDoList.childElementCount;
-
-    const button = screen.getByText('Add');
-    fireEvent.click(button);
-
-    expect(toDoList.childElementCount).toBe(length);
-  });
-
-  it('loads localStorage data', () => {
-    const sampleData = ['ToDo 1', 'ToDo 2', 'ToDo 3'];
+  it('goes to Detail page and comes back to List page', () => {
+    const sampleData = ['Todo 1', 'Todo 2'];
     localStorage.setItem('ToDoList', JSON.stringify(sampleData));
+    const TestComponent = (): JSX.Element => {
+      const { pathname } = useLocation();
+      return <div>{pathname}</div>;
+    };
+    const { container } = renderWithMemoryRouter({}, <TestComponent />);
 
-    render(<App />);
+    const header = screen.getByText('Todo List');
+    const url = screen.getByText('/');
 
-    expect(screen.getByText(sampleData[0])).toBeInTheDocument();
-    expect(screen.getByText(sampleData[1])).toBeInTheDocument();
-    expect(screen.getByText(sampleData[2])).toBeInTheDocument();
-    expect(screen.getAllByText('Delete').length).toBe(3);
+    const toDoItem = screen.getByText(sampleData[0]);
+    expect(toDoItem).toBeInTheDocument();
+    expect(toDoItem.getAttribute('href')).toBe('/detail/0');
+
+    fireEvent.click(toDoItem);
+
+    expect(header.textContent).toBe('Todo In Detail');
+    expect(url.textContent).toBe('/detail/0');
+    const goBackButton = screen.getByText('Go Back...');
+    expect(goBackButton).toBeInTheDocument();
+    const toDoText = screen.getByText(sampleData[0]);
+    expect(toDoText).toBeInTheDocument();
+    const input = screen.getByText('Delete');
+    expect(input).toBeInTheDocument();
+
+    expect(container).toMatchSnapshot();
+
+    fireEvent.click(goBackButton);
+    expect(header.textContent).toBe('Todo List');
+    const toDoList = screen.getByTestId('toDoList');
+    expect(toDoList).toBeInTheDocument();
+  });
+
+  it('deletes a todo in Detail page and comes back to List page', () => {
+    const sampleData = ['Todo 1'];
+    localStorage.setItem('ToDoList', JSON.stringify(sampleData));
+    renderWithMemoryRouter();
+
+    let toDoList = screen.getByTestId('toDoList');
+    expect(toDoList.childNodes.length).toBe(1);
+
+    const header = screen.getByText('Todo List');
+
+    const toDoItem = screen.getByText(sampleData[0]);
+    expect(toDoItem).toBeInTheDocument();
+    fireEvent.click(toDoItem);
+
+    expect(header.textContent).toBe('Todo In Detail');
+    const deleteButton = screen.getByText('Delete');
+    fireEvent.click(deleteButton);
+
+    expect(header.textContent).toBe('Todo List');
+    expect(toDoItem).not.toBeInTheDocument();
+    toDoList = screen.getByTestId('toDoList'); // testId로 조회한 ToDoList 컴포넌트의 렌더링 갱신
+    expect(toDoList.childNodes.length).toBe(0);
+    expect(localStorage.getItem('ToDoList')).toBe('[]');
+  });
+
+  it('shows NotFound page when user enters undefined URL and can go back to List page', () => {
+    const TestComponent = (): JSX.Element => {
+      const { pathname } = useLocation();
+      return <div>{pathname}</div>;
+    };
+    const { container } = renderWithMemoryRouter(
+      { initialIndex: 1, initialEntries: ['/', '/not_found'] },
+      <TestComponent />,
+    );
+
+    const url = screen.getByText('/not_found');
+
+    const header = screen.getByText('Error');
+    expect(header).toBeInTheDocument();
+    const goBackButton = screen.getByText('Go Back...');
+    expect(goBackButton).toBeInTheDocument();
+    const notFoundText = screen.getByText('Page Not Found 😱');
+    expect(notFoundText).toBeInTheDocument();
+
+    expect(container).toMatchSnapshot();
+
+    fireEvent.click(goBackButton);
+
+    expect(url.textContent).toBe('/');
+    expect(header.textContent).toBe('Todo List');
   });
 });
